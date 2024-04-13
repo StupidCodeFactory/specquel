501370348f7619ea270a030c20d54005fc487f76
yann marquet <stupidcodefactory@yanns-MacBook-Pro.local>
2024-04-13 11:38:26 +0200

    Scaffold of rspec extension for the Sequel gem

---
+++ spec/specquel_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+RSpec.describe Specquel do
+  it 'has a version number' do
+    expect(Specquel::VERSION).not_to be nil
+  end
+
+  describe 'when required' do
+    around do |example|
+      Singleton.__init__(described_class::Configuration)
+      example.run
+      Singleton.__init__(described_class::Configuration)
+    end
+
+    context 'when the #connection_arguments is set' do
+      before do
+        Specquel.configure do |config|
+          config.connection_arguments = 'sqlite://test.db'
+        end
+      end
+
+      it 'allows to query a database' do
+        expect { db.select(1).to_a }.not_to raise_error
+      end
+    end
+
+    context 'when the #connection_arguments is not set' do
+      it 'raises a useful error' do
+        expect { db }.to raise_error ArgumentError, described_class::API::NO_CONNECTION_ARGUMENTS_ERROR
+      end
+    end
+  end
+end
